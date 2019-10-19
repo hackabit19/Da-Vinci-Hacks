34f032172158be321dc65021826f2868415ccf98
Saket Gupta <saketgupta1008@gmail.com>
2019-10-20 01:32:25 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -1,14 +1,37 @@
 import nltk
+import string
+import nltk
+import pickle
+import re
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+
+def text_prepare(text):
+    """Performs tokenization and simple preprocessing."""
+    
+    replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
+    bad_symbols_re = re.compile('[^0-9a-z #+_]')
+    stopwords_set = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = replace_by_space_re.sub(' ', text)
+    text = bad_symbols_re.sub('', text)
+    text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
+    return text.strip()
+
 def get_output(q_no,answer):
-    student_response=answer.lower()
+    student_response=text_prepare(answer)
     with open("a.text") as f:
         real=[]
         for line in f:
             real.append(line)
-    text = word_tokenize(str(real[q_no]).lower())
+    text = word_tokenize(text_prepare(str(real[q_no])))
     text2=word_tokenize(student_response)
     f=nltk.pos_tag(text)
     f2=nltk.pos_tag(text2)
